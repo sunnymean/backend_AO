d8d3b9551c456dccf4721888a175e3ec4837e626
sunnymean <sryu@hist.co.kr>
2023-03-08 08:52:29 +0900

    hateos added initials

--- src/main/java/com/example/payroll/EmployeeController.java
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -1,8 +1,13 @@
 package com.example.payroll;
 
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 
 @RestController
 public class EmployeeController {
@@ -24,9 +29,18 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
-        return repository.findById(id)
+//    Employee one(@PathVariable Long id) {
+    EntityModel<Employee> one(@PathVariable Long id) {
+
+       /* return repository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException(id));*/
+        Employee employee = repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return EntityModel.of(employee, //
+                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+
     }
 
     @PutMapping("/employees/{id}")
