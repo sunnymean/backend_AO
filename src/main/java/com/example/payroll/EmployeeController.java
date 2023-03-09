53a9909d0e804666edba9ed5a418e239d3aba9a6
sunnymean <sryu@hist.co.kr>
2023-03-09 15:17:32 +0900

    swagger examples

--- src/main/java/com/example/payroll/EmployeeController.java
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -3,6 +3,11 @@ package com.example.payroll;
 //import org.springframework.hateoas.EntityModel;
 //import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,32 +15,48 @@ import java.util.List;
 //import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 //import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-
+@Tag(name = "Employees", description = "Employee 컨트롤러")
 @RestController
 public class EmployeeController {
 
     private final EmployeeRepository repository;
 
+
     public EmployeeController(EmployeeRepository repository) {
         this.repository = repository;
     }
 
-    @GetMapping("/employees")
+    @Operation(summary = "get employees", description = "모든 employees 조회 샘플", tags = "Employees", responses =
+            {@ApiResponse(responseCode = "200", description = "**오류 발생시 Status Code**<br /><br />" +
+                    "**MANDATORY_PARAM_ERROR** : mandatory parameter is empty.<br />" + "**EXTERNAL_SERVICE_ERROR** :" +
+                    " external service error.<br />" + "**INTERNAL_SERVER_ERROR** : internal server error.<br />" +
+                    "**SUCCESS** : success.<br />" + "**FAIL** : fail<br />")})
+    @GetMapping(path = "/employees", produces = {MediaType.APPLICATION_JSON_VALUE})
     List<Employee> all() {
         return repository.findAll();
     }
 
+    @Operation(summary = "create a employee", description = "employee 입력 샘플", tags = "Employees", responses =
+            {@ApiResponse(responseCode = "200", description = "**오류 발생시 Status Code**<br /><br />" +
+                    "**MANDATORY_PARAM_ERROR** : mandatory parameter is empty.<br />" + "**EXTERNAL_SERVICE_ERROR** :" +
+                    " external service error.<br />" + "**INTERNAL_SERVER_ERROR** : internal server error.<br />" +
+                    "**SUCCESS** : success.<br />" + "**FAIL** : fail<br />")})
     @PostMapping("/employees")
-    Employee newEmployee(@RequestBody Employee newEmployee) {
+    Employee newEmployee(@Parameter(name = "Employee Object", description = "object of new employees", example =
+            "589HE3") @RequestBody Employee newEmployee) {
         return repository.save(newEmployee);
     }
 
+    @Operation(summary = "get a employee", description = "one employee 조회 샘플", tags = "Employees", responses =
+            {@ApiResponse(responseCode = "200", description = "**오류 발생시 Status Code**<br /><br />" +
+                    "**MANDATORY_PARAM_ERROR** : mandatory parameter is empty.<br />" + "**EXTERNAL_SERVICE_ERROR** :" +
+                    " external service error.<br />" + "**INTERNAL_SERVER_ERROR** : internal server error.<br />" +
+                    "**SUCCESS** : success.<br />" + "**FAIL** : fail<br />")})
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
+    Employee one(@Parameter(name = "Employee Id", description = "id number of a employee", example =
+            "2") @PathVariable(name="Employee id") long id) {
 //    EntityModel<Employee> one(@PathVariable Long id) {
-
-        return repository.findById(id)
-                .orElseThrow(() -> new EmployeeNotFoundException(id));
+        return repository.findById((long) id).orElseThrow(() -> new EmployeeNotFoundException(id));
        /* Employee employee = repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
 
@@ -46,17 +67,17 @@ public class EmployeeController {
     }
 
     @PutMapping("/employees/{id}")
-    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id){
-        return repository.findById(id)
-                .map(employee -> {
-                    employee.setName(newEmployee.getName());
-                    employee.setRole(newEmployee.getRole());
-                    return repository.save(employee);
-                }).orElseGet(() -> {
-                    newEmployee.setId(id);
-                    return repository.save(newEmployee);
-                });
+    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+        return repository.findById(id).map(employee -> {
+            employee.setName(newEmployee.getName());
+            employee.setRole(newEmployee.getRole());
+            return repository.save(employee);
+        }).orElseGet(() -> {
+            newEmployee.setId(id);
+            return repository.save(newEmployee);
+        });
     }
+
     @DeleteMapping("/employees/{id}")
     void deleteEmployee(@PathVariable Long id) {
         repository.deleteById(id);
